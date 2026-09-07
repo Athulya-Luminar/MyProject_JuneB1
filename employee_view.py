c2287ee3ca2145d831f6b2e8820371412290a05c
Athulya-Luminar <pythonluminar@gmail.com>
2026-09-07 10:44:44 +0530

    Initial commit, database connection established

---
+++ employee_view.py
@@ -0,0 +1,17 @@
+import mysql.connector
+
+class DbConnect:
+    def get_connection(self):
+        try:
+            self.connection = mysql.connector.connect(
+                host="localhost",
+                user="root",
+                password="Athulya@2907",
+                database="gym_db_b1"
+            )
+            return self.connection
+        except Exception as e:
+            return None
+
+connection_instance = DbConnect()
+connection_instance.get_connection()
