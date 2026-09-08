79ede6a2c55e09d89d1a137a304acadb46cf13bb
Athulya-Luminar <pythonluminar@gmail.com>
2026-09-08 09:26:30 +0530

    Created a class for  member management

--- employee_view.py
+++ employee_view.py
@@ -13,5 +13,9 @@ class DbConnect:
         except Exception as e:
             return None
 
+class GymMemberManager(DbConnect):
+    pass
+
+
 connection_instance = DbConnect()
 connection_instance.get_connection()
